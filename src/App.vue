b4c0bdb963c50ead44bb686a30ccb81ccfb3c279
hamada <zc73s.masuwari@gmail.com>
2021-01-24 09:11:51 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <input type="text" v-model="postnumber">
+    <button @click="getAddress()">住所自動入力</button>
+    <p>Address:{{address}}</p>
   </div>
+    
 </template>
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      postnumber:"",
+      address:""
+    };
+  },
+  methods:{
+    async getAddress(){
+      const key = "eBf0Hooj6sbaPH2g6i64EInrJQerRsE7nkpQPPF";
+
+      const item = await axios.get('https://apis.postcode-jp.com/api/v4/postcodes/'+this.postnumber+'/?apikey='+key);
+
+      this.address = item.data[0].allAddress;
+    }
+  }
+}
+
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
